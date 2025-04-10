17d84f302c7b360b52d11c2878721ea956866f27
qkrtjdgks33 <qkrtjdgks49@gmail.com>
2025-04-10 12:53:15 +0900

    6주차

--- lib/presentation/main/main_screen.dart
+++ lib/presentation/main/main_screen.dart
@@ -8,8 +8,54 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  //  BoxDecoration _containerDecoration = BoxDecoration(
+  //    color: Colors.white,
+  //    borderRadius: BorderRadius.circular(15),
+  //  );
+
+  Widget _buildContainer() {
+    return Container(
+      height: 120,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color(0xFFF4F3FF),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 30, right: 30, bottom: 90),
+          child: Column(
+            spacing: 25,
+            children: [
+              SizedBox(height: 35),
+              Text('BMI CALCULATOR', style: TextStyle(fontSize: 20)),
+              Row(
+                spacing: 20,
+                children: [
+                  Expanded(child: _buildContainer()),
+                  Expanded(child: _buildContainer()),
+                ],
+              ),
+              _buildContainer(),
+              _buildContainer(),
+              SizedBox(
+                width: double.infinity,
+                height: 75,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Calculate BMI'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
